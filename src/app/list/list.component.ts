f53a3d4b87d61f1cf491b05447aa44740dbd0b87
Peter Jeffery <mr__j@hotmail.com>
2020-11-25 19:33:59 +0000

    impl device list state

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,5 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { IobrokerService } from '../iobroker.service';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+class MachineState {
+    public name: string;
+    public state: Observable<boolean>;
+
+    constructor(name: string, state: Observable<boolean>) {
+        this.name = name;
+        this.state = state;
+    }
+}
 
 @Component({
     selector: 'app-list',
@@ -9,11 +21,38 @@ import { IobrokerService } from '../iobroker.service';
 })
 export class ListComponent implements OnInit {
 
-    constructor(broker: IobrokerService) {
+    private broker: IobrokerService;
 
+    public listeners: MachineState[];
+
+    constructor(broker: IobrokerService) {
+        this.broker = broker;
+        this.listeners = [];
     }
 
     ngOnInit() {
+        this.broker.Services.subscribe({
+            next: (srvs) => {
+                this.listeners.splice(0, this.listeners.length);
+
+                const pingSrvs = srvs.filter((val, idx, list) => {
+                    return val.startsWith('ping.0.pi1');
+                });
+
+                pingSrvs.forEach(element => {
+                    const listener = this.broker.ListenOn(element);
+                    if (listener != null) {
+                        const stateHolder = new MachineState(element.substr(11), listener.pipe(
+                            map((val) => {
+                                return val == true;
+                            })
+                        ));
+
+                        this.listeners.push(stateHolder);
+                    }
+                });
+            }
+        });
     }
 
 }
