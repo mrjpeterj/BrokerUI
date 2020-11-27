2bab83f7c38372b30f5344294c61fc1f8044bbcb
Peter Jeffery <mr__j@hotmail.com>
2020-11-27 17:54:27 +0000

    code to back the update to use state coloring

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,21 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-import { IobrokerService } from '../iobroker.service';
+
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { IobrokerService } from '../iobroker.service';
 
 class MachineState {
     public name: string;
-    public state: Observable<boolean>;
+    public state: Observable<string>;
 
     constructor(name: string, state: Observable<boolean>) {
         this.name = name;
-        this.state = state;
+        this.state = state.pipe(
+            map((val) => {
+                if (val) {
+                    return 'On';
+                } else {
+                    return 'Off';
+                }
+            })
+        );
     }
 }
 
 @Component({
     selector: 'app-list',
     templateUrl: './list.component.html',
-    styleUrls: ['./list.component.sass'],
+    styleUrls: ['./list.component.scss'],
     providers: [IobrokerService]
 })
 export class ListComponent implements OnInit {
