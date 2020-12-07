63af3e8413ad652a397a35f0bf48ce9eee93680d
Peter Jeffery <mr__j@hotmail.com>
2020-12-07 09:13:32 +0000

    whitespace

--- src/app/broker/statemaker.ts
+++ src/app/broker/statemaker.ts
@@ -10,15 +10,12 @@ export class BrokerStateMaker {
     public static Create(id: string, stateDesc: ioBroker.StateCommon): BrokerState | null {
         const name = BrokerHelpers.GetString(stateDesc.name);
 
-        switch (stateDesc.type) {
-            case 'boolean':
-                return new BrokerBoolState(name, id, stateDesc.read, stateDesc.write);
-            case 'number':
-                return new BrokerIntState(name, id, stateDesc.min, stateDesc.max, stateDesc.read, stateDesc.write);
-            case 'string':
-                return null;
-            default:
-                return null;
+        if (stateDesc.type === 'boolean' || stateDesc.role === 'button') {
+            return new BrokerBoolState(name, id, stateDesc.read, stateDesc.write);
+        } else if (stateDesc.type === 'number') {
+            return new BrokerIntState(name, id, stateDesc.min, stateDesc.max, stateDesc.read, stateDesc.write);
         }
+
+        return null;
     }
 }
